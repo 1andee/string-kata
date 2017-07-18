69b94741dd62d613b621bbdf4cdd54a19881c4ca
Ian Duke <ian.c.duke@gmail.com>
2017-07-17 21:58:44 -0400

    Updated last attempt with comments

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,8 +4,11 @@ module StringCalculator
     delim, string = extract_delim(string)
     string.gsub(/\n/, delim)
     return string.to_i unless string.include?delim
+  # return to_i_with_raise(string) unless string.include?delim
     digit_strings = string.split(delim)
     digit_strings.inject(0) { |t,s| t + s.to_i }
+  # COMMENTED OUT LINE 11 BECAUSE IT DOESN'T WORK
+  # digit_strings.inject(0) { |t,s| t + s.to_i_with_raise }
   end
 
   def self.extract_delim(string)
