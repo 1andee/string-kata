4a77b8e82d9f09d47f900f4344fa011d5ef99f1f
Ian Duke <ian.c.duke@gmail.com>
2017-07-23 00:36:07 -0400

    Passing all tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,12 +3,9 @@ module StringCalculator
   def self.add(string)
     delim, string = extract_delim(string)
     string.gsub(/\n/, delim)
-    return string.to_i unless string.include?delim
-  # return to_i_with_raise(string) unless string.include?delim
+    return to_i_with_raise(string) unless string.include?delim
     digit_strings = string.split(delim)
-    digit_strings.inject(0) { |t,s| t + s.to_i }
-  # COMMENTED OUT LINE 11 BECAUSE IT DOESN'T WORK
-  # digit_strings.inject(0) { |t,s| t + s.to_i_with_raise }
+    digit_strings.inject(0) { |t,s| t + to_i_with_raise(s) }
   end
 
   def self.extract_delim(string)
@@ -18,9 +15,10 @@ module StringCalculator
     [ delim, string[4..-1] ]
   end
 
-  # def self.to_i_with_raise(string)
-  #   i = string.to_i_with_raise
-  #   raise "Cannot handle negative numbers" if i < 0
-  # end
+  def self.to_i_with_raise(string)
+    i = string.to_i
+    raise "Cannot handle negative numbers" if i < 0
+    i
+  end
 
 end
